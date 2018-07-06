17a702efbeb052e22c77f812c567fefe7e5c3f2f
Iurii_Plevako <orbot90@gmail.com>
2018-07-06 17:54:11 +0300

    Prepared template of UI for sudoku solver

---
+++ src/main/kotlin/ru/orbot90/sudoku/SudokuApplication.kt
@@ -0,0 +1,21 @@
+package ru.orbot90.sudoku
+
+import javafx.application.Application
+import javafx.fxml.FXMLLoader
+import javafx.scene.Parent
+import javafx.scene.Scene
+import javafx.stage.Stage
+
+class SudokuApplication : Application() {
+    override fun start(primaryStage: Stage) {
+        val loader = FXMLLoader(this.javaClass.getResource("/fxml/main_window.fxml"))
+        val mainDialog: Parent = loader.load()
+        primaryStage.title = "Sudoku Solver"
+        primaryStage.scene = Scene(mainDialog, 600.0, 700.0)
+        primaryStage.show()
+    }
+}
+
+fun main(args: Array<String>) {
+    Application.launch(SudokuApplication::class.java)
+}
---
+++ src/main/kotlin/ru/orbot90/sudoku/SudokuMainWindowController.kt
@@ -0,0 +1,20 @@
+package ru.orbot90.sudoku
+
+import javafx.fxml.FXML
+import javafx.scene.layout.GridPane
+
+class SudokuMainWindowController {
+
+    @FXML
+    private lateinit var sudokuPane: GridPane
+
+    @FXML
+    fun initialize() {
+
+    }
+
+    @FXML
+    fun loadFromFile() {
+
+    }
+}
