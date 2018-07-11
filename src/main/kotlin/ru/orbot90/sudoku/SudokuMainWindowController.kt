689093914a84691e94022f0b2613e26525c79002
Iurii_Plevako <orbot90@gmail.com>
2018-07-11 19:26:55 +0300

    Added the opportunity to enter sudoku cells values in UI

--- src/main/kotlin/ru/orbot90/sudoku/SudokuMainWindowController.kt
+++ src/main/kotlin/ru/orbot90/sudoku/SudokuMainWindowController.kt
@@ -1,7 +1,10 @@
 package ru.orbot90.sudoku
 
 import com.google.gson.Gson
+import javafx.beans.value.ObservableValue
+import javafx.event.EventHandler
 import javafx.fxml.FXML
+import javafx.scene.control.TextField
 import javafx.scene.layout.GridPane
 import javafx.scene.layout.VBox
 import javafx.scene.text.Text
@@ -12,15 +15,50 @@ class SudokuMainWindowController {
     private lateinit var sudokuPane: GridPane
     private lateinit var sudokuCellBoxes: Array<Array<VBox>>
     private val sudokuProcessor = SudokuProcessor()
+    private var selectedBox: VBox? = null
 
-    @FXML
     fun initialize() {
         sudokuCellBoxes = Array(9) {Array(9) {
             val box = VBox()
             box.styleClass.add("sudokucell")
             box.children.add(Text())
+            box.onMouseClicked = EventHandler { event ->
+                val selectedBoxFirstChild = selectedBox?.children?.get(0)
+                if (selectedBoxFirstChild is TextField) {
+                    selectedBox?.children?.set(0, Text(selectedBoxFirstChild.text))
+                }
+                val clickedBox = event.source as VBox
+                selectedBox = clickedBox
+                val cellText: String
+
+                cellText = if (clickedBox.children.size > 0) {
+                    val text: Text = clickedBox.children[0] as Text
+                    text.text
+                } else {
+                    ""
+                }
+
+                val textInput = TextField(cellText)
+                clickedBox.children[0] = textInput
+                textInput.styleClass.add("sudokucell")
+                textInput.requestFocus()
+                textInput.textProperty().addListener { observable: ObservableValue<out String>, oldValue: String, newValue: String ->
+                    if (newValue.length > 1) {
+                        textInput.text = oldValue
+                    }
+                }
+                textInput.onAction = EventHandler {
+                    val cellText = textInput.text
+                    if (cellText.isNotBlank()) {
+                        clickedBox.children[0] = Text(cellText)
+                    } else {
+                        clickedBox.children[0] = null
+                    }
+                }
+            }
             box
         }}
+
         for (rowIndex in 0..8) {
             for (columnIndex in 0..8) {
                 val cellBox = sudokuCellBoxes[rowIndex][columnIndex]
@@ -29,29 +67,15 @@ class SudokuMainWindowController {
         }
     }
 
-    @FXML
-    fun loadFromFile() {
-        val sudokuArr = sudokuProcessor.retrieveSudokuFromFile("sudoku.jsn")
-        for (rowIndex in 0..8) {
-            for (columnIndex in 0..8) {
-                val cellValue = sudokuArr[rowIndex][columnIndex]
-                if (cellValue != 0) {
-                    val text = this.sudokuCellBoxes[rowIndex][columnIndex].children[0] as Text
-                    text.text = cellValue.toString()
-                }
-            }
-        }
-    }
-
-    @FXML
     fun solveSudoku() {
         val valuesArr = Array(9) {
             IntArray(9)
         }
         for (rowIndex in 0..8) {
             for (columnIndex in 0..8) {
-                val text = this.sudokuCellBoxes[rowIndex][columnIndex].children[0] as Text
-                val textVal = text.text
+                val text = this.sudokuCellBoxes[rowIndex][columnIndex].children[0]
+                val textVal = (if (text is Text) text.text else
+                    (if (text is TextField) text.text else ""))
                 if (textVal.isNotBlank()) {
                     val cellValue = Integer.valueOf(textVal)
                     valuesArr[rowIndex][columnIndex] = cellValue
@@ -64,8 +88,13 @@ class SudokuMainWindowController {
         for (rowIndex in 0..8) {
             for (columnIndex in 0..8) {
                 val cellValue = resultArr[rowIndex][columnIndex]
-                val text = this.sudokuCellBoxes[rowIndex][columnIndex].children[0] as Text
-                text.text = cellValue.toString()
+                val text = this.sudokuCellBoxes[rowIndex][columnIndex].children[0]
+                if (text is Text) {
+                    text.text = cellValue.toString()
+                } else {
+                    val textElement = Text(cellValue.toString())
+                    this.sudokuCellBoxes[rowIndex][columnIndex].children[0] = textElement
+                }
             }
         }
     }
